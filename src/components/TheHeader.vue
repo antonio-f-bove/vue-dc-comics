9f7761afdf63de4b0e29e0629450d3b64c9b84bb
AntonioFBove <antoniofbove@gmail.com>
2022-01-27 22:07:34 +0100

    TheHeader layout

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,25 @@
 <template>
   <header>
-    <div class="logo-container">
-      <img src="../assets/dc-logo.png" alt="">
-    </div>
-    <nav>
-      <ul>
-        <li></li>
-      </ul>
+    <div class="container">
+      <div class="logo-container">
+        <img src="../assets/dc-logo.png" alt="">
+      </div>
 
-    </nav>
+      <nav>
+        <ul>
+          <li>CHARACTERS</li>
+          <li>COMICS</li>
+          <li>MOVIES</li>
+          <li>TV</li>
+          <li>GAMES</li>
+          <li>COLLECTIBLES</li>
+          <li>VIDEOS</li>
+          <li>FANS</li>
+          <li>NEWS</li>
+          <li>SHOP</li>
+        </ul>
+      </nav>
+    </div>
   </header>  
 </template>
 
@@ -19,5 +30,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
+
+.container {
+  width: $container-width;
+  margin: 0 auto;
+  padding: 20px 0;
+  height: 150px;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+nav {
+  height: 100%;
+
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: flex-end;
+    height: 100%;
+
+      li {
+        list-style: none;
+        margin: 0 14px;
+        font-size: 14px;
+        height: 100%;
+        border-bottom: solid 5px transparent;
+        
+        display: flex;
+        align-items: center;
+
+        &:hover {
+          border-bottom: 5px solid $dc-blue;
+          color: $dc-blue;
+        }
+      }
+  }
+}
+
 
 </style>
