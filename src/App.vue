1ba038e835cb277ea627f913f3425a895dc46f61
AntonioFBove <antoniofbove@gmail.com>
2022-01-27 17:47:21 +0100

    components initialisation

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <the-header></the-header>
+    <the-jumbo></the-jumbo>
+    <the-main></the-main>
+    <the-footer></the-footer>
+    
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+import TheFooter from './components/TheFooter.vue'
+import TheMain from './components/TheMain.vue'
+import TheJumbo from './components/TheJumbo.vue'
+
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    TheFooter,
+    TheMain,
+    TheJumbo
+  },
+};
+</script>
+
+<style lang="scss">
+@import './style/main.scss';
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <div class="logo-container">
+      <img src="../assets/dc-logo.png" alt="">
+    </div>
+    <nav>
+      <ul>
+        <li></li>
+      </ul>
+
+    </nav>
+  </header>  
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
