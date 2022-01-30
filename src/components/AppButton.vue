5503bfc5091e82c35229396e5bfb5c0d47082cc0
AntonioFBove <antoniofbove@gmail.com>
2022-01-30 20:39:18 +0100

    TheMain: dynamic comics & layout

---
+++ src/components/AppButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+div {
+  background-color: $dc-blue;
+  display: inline-block;
+  padding: .5em 1em;
+  font-weight: bold;
+  font-size: 22px;
+  color: #fff;      
+}
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,20 +1,33 @@
 <template>
   <main>
     <div class="container">
-      <div class="card">
-        <div class="wrapper">
-          <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX">
+      <app-button id="current">CURRENT SERIES</app-button>
+
+      <div class="row">
+        <div v-for="(card, index) in cards" :key="index" class="card">
+          <div class="wrapper">
+            <img :src="card.thumb">
+          </div>
+          <p>{{ card.series.toUpperCase() }}</p>
         </div>
-        <p>Lorem, ipsum dolor.</p>
+      </div>
+
+      <div class="row">
+        <app-button id="load">LOAD MORE</app-button>
       </div>
     </div>
   </main>
 </template>
 
 <script>
+import AppButton from './AppButton.vue'
+
 export default {
   props: {
     cards: Array
+  },
+  components: {
+    AppButton
   }
 }
 </script>
@@ -22,35 +35,54 @@ export default {
 <style lang="scss" scoped>
 @import '../style/variables.scss';
 
-
 main {
   background: #333;
 
   .container {
     width: $container-width;
     margin: 0 auto;
+    padding: 24px 0 12px;
+    position: relative;
 
-    .card {
-      width: calc(100% / 6 - 6 * 12px);
-      padding: 12px 6px;
-
-      .wrapper {
-        width: 192px;
-        height: 192px;
-
-        img {
-          height: 100%;
-          width: 100%;
-          object-fit: cover;
-          object-position: top;
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+  
+      .card {
+        width: calc(100% / 6);
+        padding: 24px 10px;
+  
+        .wrapper {
+          width: 180px;
+          height: 180px;
+          margin-bottom: 12px;
+  
+          img {
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+            object-position: top;
+          }
+        }
+  
+        p {
+          color: #fff;
+          font-size: 14px;
+          font-weight: bold;
         }
       }
+    }
 
-      p {
-        color: #fff;
-        font-size: 14px;
-        width: fit-content;
-      }
+    #current {
+      position: absolute;
+      top: 0;
+      transform: translateY(-50%);
+    }
+
+    #load {
+      font-size: 12px;
+      padding: .75em 4em;
+      margin: 0 auto;
     }
   }
 }
